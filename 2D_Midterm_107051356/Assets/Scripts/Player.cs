b985f7fe321605cb2b9a438548ffbb63602454cf
Kuo0418 <71679871+Kuo0418@users.noreply.github.com>
2020-11-15 03:43:10 +0800

    程式

---
+++ 2D_Midterm_107051356/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public GameObject end;
+    public Text textcount;
+    public int count;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "傳送門")
+        {
+            print("碰到傳送門");
+            end.SetActive(true);
+        }
+
+        if (collision.tag == "鑽石")
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textcount.text = "鑽石數量：" + count;
+        }
+    }
+}
